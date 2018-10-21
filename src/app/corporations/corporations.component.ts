a9fc71d83f56c9a1740b6b60516ba8fc6395eb75
Ruby Abutaleb <rubyabutaleb1@csu.fullerton.edu>
2018-10-20 20:23:16 -0700

    Add files via upload

---
+++ src/app/corporations/corporations.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-corporations',
+  templateUrl: './corporations.component.html',
+  styleUrls: ['./corporations.component.css']
+})
+export class CorporationsComponent {
+}
