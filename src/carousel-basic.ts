eb5f581aa72c3f5312c73721e3a4faf497723305
Ruby Abutaleb <rubyabutaleb1@csu.fullerton.edu>
2018-10-23 09:56:11 -0700

    Delete carousel-basic.ts

--- src/carousel-basic.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({selector: 'ngbd-carousel-basic', templateUrl: './carousel-basic.html'})
-export class NgbdCarouselBasic {
-  images = ['assets/images/blackLivesMatter1.jpg', 'assets/images/climateChange.jpg', 'assets/images/womensMarch.jpg'];
-}
